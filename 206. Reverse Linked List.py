c987f39e10077d2e0fce05a4880600e62f58b48a
Chaitanya Jadhav <chaitanyajadhav911@gmail.com>
2023-12-23 20:13:49 +0530

    41 ms

---
+++ 206. Reverse Linked List.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next: return head
+
+        prev = None
+        curr = head
+
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+
+        head = prev 
+        return head
+
