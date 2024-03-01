f7d6d4b91f086d4d54dff5c9e9f47e8fd165d342
Chaitanya Jadhav <chaitanyajadhav911@gmail.com>
2024-03-01 21:59:33 +0530

    29 ms

---
+++ 2864. Maximum Odd Binary Number.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximumOddBinaryNumber(self, s: str) -> str:
+        ones_count = s.count("1")
+        zeros_count = len(s) - ones_count
+
+        return "1" * (ones_count - 1) + "0" * zeros_count + "1"
